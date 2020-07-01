1c7d73c537d70d1650773eb86bcb38b40d070c2c
LoriWhite24 <lori.r.white.24@gmail.com>
2020-07-01 15:36:46 -0400

    update of book dao

--- java/com/cognixia/jump/dao/BookDao.java
+++ java/com/cognixia/jump/dao/BookDao.java
@@ -1,8 +1,12 @@
 package com.cognixia.jump.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.cognixia.jump.connection.ConnectionManager;
 import com.cognixia.jump.models.Book;
@@ -17,6 +21,8 @@ public class BookDao {
 	//private static String DELETE_BOOK = "delete from product where id = ?";
 	private static String UPDATE_BOOK = "update product set item = ?, qty = ?, description = ? where id = ?";
 
+	private static String SELECT_ALL_BOOKS = "select * from book";
+	
 public boolean addBook(Book book) {
 		
 		try(PreparedStatement bstmt = conn.prepareStatement(INSERT_BOOK)) {
@@ -38,7 +44,7 @@ public boolean addBook(Book book) {
 		return false;
 	}
 
-public boolean updateProduct(Book book) {
+public boolean updateBook(Book book) {
 	
 	try (PreparedStatement bstmt = conn.prepareStatement(UPDATE_BOOK)) {
 
@@ -59,7 +65,37 @@ public boolean updateProduct(Book book) {
 	return false;
 }
 	
+public List<Book> getAllBooks () {
 	
+	// create a list of books
+	
+	
+	List<Book> allBooks = new ArrayList<Book>();
+	
+	try(PreparedStatement pstmt = conn.prepareStatement(SELECT_ALL_BOOKS);
+			ResultSet rs = pstmt.executeQuery() ) {
+		
+		while(rs.next()) {
+			
+			String isbn = rs.getString("isbn");
+			String title = rs.getString("title");
+			String descr = rs.getString("descr");
+			boolean rented = rs.getBoolean("rented");
+			Date added_to_library = rs.getDate("added_to_library");
+			
+			
+			allBooks.add(new Book(isbn, title, descr, rented,added_to_library));
+		}
+		
+		
+	} catch(SQLException e) {
+		
+		e.printStackTrace();
+	}
+	
+	return allBooks;
+	
+}
 	
 	
 }
