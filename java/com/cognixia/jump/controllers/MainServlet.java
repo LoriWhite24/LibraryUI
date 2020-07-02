b7f1ff497bd58d5c2cf748cf98d3ab6b3e1d7904
kbebel97 <63519535+kbebel97@users.noreply.github.com>
2020-07-02 13:53:20 -0400

    Update MainServlet.java

--- java/com/cognixia/jump/controllers/MainServlet.java
+++ java/com/cognixia/jump/controllers/MainServlet.java
@@ -1,138 +1,75 @@
-package com.cognixia.jump.controllers;
-
-import java.io.IOException;
-
-import java.sql.SQLException;
-import java.util.List;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.cognixia.jump.connection.ConnectionManager;
-import com.cognixia.jump.dao.*;
-import com.cognixia.jump.models.Librarian;
-import com.cognixia.jump.models.User;
-
-
-@WebServlet("/")
-public class MainServlet extends HttpServlet {
-	
-	private static final long serialVersionUID = 275L;
-	private Userdao userdao;
-	private LibrarianDao librarianDao;
-
-	
-	public void init() {
-		 userdao = new Userdao();
-		 librarianDao = new LibrarianDao();
-	}
-
-
-	public void destroy() {
-		
-		try {
-			ConnectionManager.getConnection().close();
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" isELIgnored="false"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Login</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+<script type="text/javascript" 
+	src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.0/dist/jquery.validate.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="jumbotron jumbotron-fluid">
+			<div class="container">
+				<h1 class="display-4">Login</h1>
+				<p class="lead">Please enter your username and password below.</p>
+			</div>
+		</div>
+		<form action="Authentication" method="post">
+			<label>Username</label>
+			&nbsp;&nbsp;
+			<input type="text" id="user" name="user">
+			<br><br>
+			<label>Password</label>
+			&nbsp;&nbsp;
+			<input type="password" id="pwd" name="pwd">
+			<br><span style="color:red;"> ${ message } </span>
+			<br><br>
+			<input type="submit" value="Login" class="btn btn-primary">
+			 <input type="button" value="sign-up" name="sign-up"
+    			onclick="openPage('librarian-user-signup.jsp')"/>
+    			
+    		<script type="text/javascript">
+ 				function openPage(pageURL)
+ 					{
+ 						window.location.href = pageURL;
+					 }
+			</script>
 			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException {
-		
-		doGet(request, response);
-	}
-
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException {
-		
-		String action = request.getServletPath();
-		
-		switch (action) {
-		case "/Login":
-			login(request, response);
-			break;
-		case "/Authentication":
-			authentication(request, response);
-			break;
-		case "/addPatron":
-			addPatron(request, response);
-			break;
-		case "/addLibrarian":
-			addLibrarian(request, response);
-			break;
+			<script type="text/javascript">
 			
-		default:
-			
-			response.sendRedirect("/");
-			break;
-		}
-			
-	}
-	
-	private void login(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException {
 		
-		
-		RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
-		dispatcher.forward(request, response);
-	}
-
-	
-	
-	private void authentication(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException {
-		String username = request.getParameter("user");
-		String password = request.getParameter("pwd");
-		String destPage = "login.jsp";
-		Librarian librarian = librarianDao.getLibrarianByNameAndPassword(username, password);
-		User user = userdao.getPatronByPasswordAndUsername(username, password);
-		HttpSession session = request.getSession();
-		if(librarian != null) {
-			session.setAttribute("user", librarian);
-			destPage = "librarian-menu.jsp";
-			request.setAttribute("message", "");
-		} else if(user != null) {
-			session.setAttribute("user", user);
-			destPage = "user-menu.jsp";
-			request.setAttribute("message", "");
-		}
-		else {
-			String message = "Invalid username/password";
-			request.setAttribute("message", message);
-		} 
-		
-		RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
-		dispatcher.forward(request, response);
-		
-	}
-	
-	
-	private void addPatron(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String firstname = request.getParameter("fname");
-		String lastname = request.getParameter("lname");
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-		userdao.addPatron(firstname, lastname, username, password);
-		response.sendRedirect("PatronLogin");
-		
-		
-	}
-	private void addLibrarian(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException {
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-		librarianDao.addLibrarian(new Librarian(0, username, password));
-		response.sendRedirect("LibrarianLogin");
-
-	
-	}
-}
+				$(document).ready(function() {
+					$("#loginForm").validate({
+						rules: {
+							user: {
+								required: true,
+								user: true
+							},
+					
+							pwd: "required",
+						},
+						
+						messages: {
+							user: {
+								required: "Please enter your username",
+								user: "Please enter a valid username"
+							},
+							
+							pwd: "Please enter your pasword"
+						}
+					});
+				
+				});
+			</script>
+		</form>
+	</div>
+</body>
+</html>
