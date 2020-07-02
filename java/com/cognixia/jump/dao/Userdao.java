91beecdeeb7308c7a65e10100b77d15bd5c49e25
LoriWhite24 <lori.r.white.24@gmail.com>
2020-07-01 22:14:19 -0400

    Update UserDao

--- java/com/cognixia/jump/dao/Userdao.java
+++ java/com/cognixia/jump/dao/Userdao.java
@@ -3,7 +3,10 @@ package com.cognixia.jump.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.cognixia.jump.connection.ConnectionManager;
 import com.cognixia.jump.models.User;
@@ -19,9 +22,78 @@ public class Userdao  {
 
 	private static String DELETE_PATRON = "delete from patron where patron_id = ?";
 	private static String UPDATE_PATRON = "update patron set first_name = ?, last_name = ?, username = ?, password = ? where patron_id = ?";
+	private static String SELECT_ALL_PATRONS = "select * from patron";
+	private static String SELECT_PATRON_BY_ID = "select * from patron where password = ? and username = ?";
+	private static String INSERT_PATRON = "insert into patron(first_name, last_name, username, password, account_frozen) values(?, ?, ?, ?, ?)";
 
-
+	public List<User> getAllPatrons() {
+		
+		List<User> allPatrons = new ArrayList<User>();
+		
+		try(PreparedStatement pstmt = conn.prepareStatement(SELECT_ALL_PATRONS);
+				ResultSet rs = pstmt.executeQuery() ) {
+			
+			while(rs.next()) {
+				
+				int id = rs.getInt("patron_id");
+				String first_name = rs.getString("first_name");
+				String last_name = rs.getString("last_name");
+				String user_name = rs.getString("username");
+				long password = rs.getLong("password");
+				boolean account_frozen = rs.getBoolean("account_frozen");
+				allPatrons.add(new User(id, first_name, last_name, user_name, password, account_frozen));
+				
+			}
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return allPatrons;
+	}
 	
+	public User getPatronByPasswordAndUsername(String username, String password) {
+		
+		User user = null;
+		try(PreparedStatement pstmt = conn.prepareStatement(SELECT_PATRON_BY_ID)) {
+			pstmt.setString(1, username);
+			pstmt.setString(2, password);
+			
+			ResultSet rs = pstmt.executeQuery();
+			
+			// if product found, if statement run, if not null returned as product
+			if(rs.next()) {
+				user = new User(rs.getInt("patron_id"), rs.getString("first_name"), rs.getString("last_name"), rs.getString("username"), rs.getLong("password"), rs.getBoolean("account_frozen"));
+			}
+			
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return user;
+		
+		
+	}
+	public boolean addPatron(String firstname, String lastname, String username, String password) {
+		
+		try(PreparedStatement pstmt = conn.prepareStatement(INSERT_PATRON)) {
+			
+			pstmt.setString(1, firstname);
+			pstmt.setString(2, lastname);
+			pstmt.setString(3, username);
+			pstmt.setString(4,password);
+			
+			// at least one row added
+			if(pstmt.executeUpdate() > 0) {
+				return true;
+			}
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 	
 	public boolean deletePatron(int patron_id) {
 		
